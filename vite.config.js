0384cf9075dd613f2da68f18fa2968bb1054f012
Vinzboiz <binh509101@gmail.com>
2025-02-17 18:54:55 +0700

    Fix Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/VSF-Website/", // Đặt đúng theo tên repository của bạn
+  base: "/Website-V.S.F/", // Thay bằng tên repo của bạn
 });
